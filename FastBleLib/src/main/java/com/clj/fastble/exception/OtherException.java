d9600b633d0b1ace0fcc0f93c0ea633e73c98454
sulanc <suchengkuaile@126.com>
2019-08-07 16:28:25 +0800

    更新

--- FastBleLib/src/main/java/com/clj/fastble/exception/OtherException.java
+++
@@ -1,8 +0,0 @@
-package com.clj.fastble.exception;
-
-
-public class OtherException extends BleException {
-    public OtherException(String description) {
-        super(GATT_CODE_OTHER, description);
-    }
-}
